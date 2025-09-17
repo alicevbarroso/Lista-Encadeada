bfc725f4f5d694341d08b52f61db5510b9ca2584
Alice Barroso <168374846+alicevbarroso@users.noreply.github.com>
2025-09-16 23:48:42 -0300

    Create listaS.c

---
+++ listaS.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct lista{
+    int dado;
+    struct lista *link;
+} no;
+
+no *crialista(int n){
+    no *ini, *p, *ult;
+    int valor;
+    ini = ult = NULL;
+   
+    for (int i = 1; i <= n; i++){
+        printf("\n Digite o valor %d da lista: ", i);
+        scanf("%d", &valor);
+        p = (no*) malloc(sizeof(no));
+        p -> dado = valor;
+        p -> link = NULL;
+        if (ult != NULL){
+            ult -> link = p;
+        } else {
+            ini = p;
+        } ult = p;
+    }
+    return ini;
+}
+
+void escrevelista (no *p){
+    while (p != NULL){
+        printf("%d \t",p -> dado);
+        p = p -> link;
+    }
+}
+
+int main (){
+    no *primeira;
+    int n;
+    printf("Criando uma lista encadeada");
+    do {
+        printf("\n Entre com o número de nois: ");
+        scanf("%d", &n);
+    } while (n < 0);
+   
+    primeira = crialista(n);
+    if (primeira != NULL){
+        printf("\n Lista criada");
+        printf("\n");
+        escrevelista(primeira);
+    } else {
+        printf("\n Lista vazia");
+    }
+    return(0);
+}
